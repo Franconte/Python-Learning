843d56144157aae7ffda4c4efb8048dbc6cba03d
Francisco Contreras <franciscont32@gmail.com>
2024-05-25 19:06:28 -0400

    -m "Primer archivo"

---
+++ 01-helloWorld.py
@@ -0,0 +1,21 @@
+#Comentarios de una sola linea 
+
+print("hello world") 
+print('Hola python, comillas simples')
+
+
+"""
+Comentario 
+de varias 
+lineas 
+ 
+"""
+#Type(), funcion que define el tipo de dato a consultar
+
+#consultando datos 
+print(type("Esto es una cadena de caracteres")) #Tipo String(str)
+print(type(6)) #Tipo Entero (int)
+print(type(6.4)) #Tipo 'Float'  (decimal)
+print(type(1+ 4j)) #Tipo complex (dato complejo)
+print(type(True)) #Tipo Boolean (bool)
+
